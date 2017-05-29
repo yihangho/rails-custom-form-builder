57dcc62cbb66e27533a90bca56a95bda05c32eb5
Yihang Ho <hoyihang5@gmail.com>
2017-05-29 18:53:02 +0800

    Override text fields methods

--- app/helpers/bootstrap_form_builder.rb
+++ app/helpers/bootstrap_form_builder.rb
@@ -1,4 +1,12 @@
 class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
+  %w(text_field email_field password_field).each do |selector|
+    class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+      def #{selector}(method, options = {})
+        super(method, insert_class("form-control", options))
+      end
+    RUBY_EVAL
+  end
+
   def errors(method)
     object.errors.full_messages_for(method).map { |m| help_block(m) }.join.html_safe
   end
